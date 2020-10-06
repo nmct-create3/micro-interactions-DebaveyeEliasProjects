e02228ddb73259ee06b674df554b3e7cbcc5d842
DebaveyeEliasProjects <55786829+DebaveyeEliasProjects@users.noreply.github.com>
2020-10-06 10:57:38 +0200

    working password eye

--- script/app.js
+++ script/app.js
@@ -4,6 +4,24 @@ function handleFloatingLabel() {
 
 function handlePasswordSwitcher() {
     
+    let checkbox = document.querySelector(`.c-password__hidden-chkbx`);
+    let passwordInput = document.querySelector(`.c-password_input`);
+    checkbox.addEventListener(`change`, function(){
+
+        if(checkbox.checked){
+            passwordInput.type = "password";
+            
+            console.log("checked")
+        }
+        else{
+            passwordInput.type = "text";
+            console.log("unchecked")
+        }
+        
+
+
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', function () {
